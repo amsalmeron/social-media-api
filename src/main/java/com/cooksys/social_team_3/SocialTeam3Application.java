317c78f044856cb3e37a33a32e9bda53522e4c66
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-01-03 18:30:44 +0100

    Create: Spring Project, database, mapstruct

---
+++ src/main/java/com/cooksys/social_team_3/SocialTeam3Application.java
@@ -0,0 +1,13 @@
+package com.cooksys.social_team_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialTeam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialTeam3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooksys/social_team_3/SocialTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.social_team_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialTeam3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
