51aecbc1886e3ff048534eee43b2ae8620bcb71a
Martin Hu <martin.m.hu89@gmail.com>
2023-01-04 15:18:50 -0500

    Annotations for repository files, proper imports, and interface class extensions added

--- src/main/java/com/cooksys/social_team_3/repositories/TweetRepository.java
+++ src/main/java/com/cooksys/social_team_3/repositories/TweetRepository.java
@@ -1,5 +1,11 @@
 package com.cooksys.social_team_3.repositories;
 
-public interface TweetRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cooksys.social_team_3.entities.Tweet;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
 
 }
