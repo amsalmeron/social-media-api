f025918bb4839282629e38085e44009ed3f0c666
Martin Hu <martin.m.hu89@gmail.com>
2023-01-04 11:16:33 -0500

    entity classes completed

--- src/main/java/com/cooksys/social_team_3/entities/Tweet.java
+++ src/main/java/com/cooksys/social_team_3/entities/Tweet.java
@@ -1,5 +1,71 @@
 package com.cooksys.social_team_3.entities;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Tweet {
 
+	@Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private User author;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp posted;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    private Tweet inReplyTo;
+
+    @OneToMany(mappedBy = "replies")
+    private List<Tweet> replies;
+
+    @ManyToOne
+    private Tweet repostOf;
+
+    @OneToMany(mappedBy = "repostOf")
+    private List<Tweet> reposts;
+
+    @ManyToMany(mappedBy = "likedTweets")
+    private List<User> likes;
+
+    @ManyToMany
+    @JoinTable(
+    		name = "tweet_hashtags",
+    		joinColumns = @JoinColumn(name = "tweet_id"),
+    		inverseJoinColumns = @JoinColumn(name = "hashtag_id"))
+    private List<Hashtag> hashtags;
+
+    @ManyToMany
+    @JoinTable(
+    		name = "user_mentions",
+    		joinColumns = @JoinColumn(name = "tweet_id"),
+    		inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> usersMentioned;
 }
