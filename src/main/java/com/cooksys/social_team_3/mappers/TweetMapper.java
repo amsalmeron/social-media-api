323dcd976cef63f1734b254f91217463bc66da14
Martin Hu <martin.m.hu89@gmail.com>
2023-01-05 13:34:37 -0500

    added comments for endpoints 45/46/47.  implementation for endpoints 60/62 completed, ready for review

--- src/main/java/com/cooksys/social_team_3/mappers/TweetMapper.java
+++ src/main/java/com/cooksys/social_team_3/mappers/TweetMapper.java
@@ -1,8 +1,15 @@
 package com.cooksys.social_team_3.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = { UserMapper.class })
-public class TweetMapper {
+import com.cooksys.social_team_3.entities.Tweet;
+import com.cooksys.social_team_3.dtos.TweetResponseDto;
 
+@Mapper(componentModel = "spring", uses = { UserMapper.class })
+public interface TweetMapper {
+
+	List<TweetResponseDto> entitiesToDtos(List<Tweet> tweet);
+	
 }
