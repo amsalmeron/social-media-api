a25a83f5a57e809b15048df42a3d49867ffa4a68
Martin Hu <martin.m.hu89@gmail.com>
2023-01-05 13:05:29 -0500

    Endpoints for usernameExists, hashtagExists, and usernameIsAvailable completed for review

--- src/main/java/com/cooksys/social_team_3/services/ValidateService.java
+++ src/main/java/com/cooksys/social_team_3/services/ValidateService.java
@@ -2,4 +2,10 @@ package com.cooksys.social_team_3.services;
 
 public interface ValidateService {
 
+	boolean usernameExists(String username);
+
+	boolean hashtagExists(String label);
+	
+	boolean usernameIsAvailable(String username);
+	
 }
