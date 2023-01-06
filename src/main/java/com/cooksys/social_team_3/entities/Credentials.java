dd689f797bcde34b5d58ccaa8d841560d9cbca55
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-01-06 21:20:45 +0100

    Update: entity credentials

--- src/main/java/com/cooksys/social_team_3/entities/Credentials.java
+++ src/main/java/com/cooksys/social_team_3/entities/Credentials.java
@@ -1,11 +1,8 @@
 package com.cooksys.social_team_3.entities;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
