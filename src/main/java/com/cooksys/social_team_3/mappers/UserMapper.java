159cd2f12b5d538fc0a96a7cf74cb0fedab0f358
SPatel144 <fweep321@gmail.com>
2023-01-06 14:13:03 -0500

    Cleaned last PR, UserService should work now

--- src/main/java/com/cooksys/social_team_3/mappers/UserMapper.java
+++ src/main/java/com/cooksys/social_team_3/mappers/UserMapper.java
@@ -8,9 +8,6 @@ import org.mapstruct.Mapping;
 
 import java.util.List;
 
-import com.cooksys.social_team_3.dtos.UserResponseDto;
-import com.cooksys.social_team_3.entities.User;
-
 @Mapper(componentModel = "spring", uses = { ProfileMapper.class, CredentialsMapper.class })
 public interface UserMapper {
 
